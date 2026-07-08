6efc9d47f732c091d3bb1c2efab1badaf324da2a
aryaksinghh <aryaksingh202@gmail.com>
2026-07-08 12:38:40 +0530

    solved the prisma query engine error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   images:{
     domains:["kottke.org", "sothebys-com.brightspotcdn.com", "lh3.googleusercontent.com", "avatars.githubusercontent.com", "api.dicebear.com", "wordlift.io"]
-  }
+  },
+  devIndicators: false,
+    experimental: {
+        authInterrupts: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
