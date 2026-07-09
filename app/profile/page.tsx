df1d6746dfc00fffd7b8b7bfc464a12d683b1753
aryaksinghh <aryaksingh202@gmail.com>
2026-07-09 11:13:42 +0530

    added debugging logs

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,21 @@
 import Profile from "./profileui"
-import {prisma} from "@/prisma_setup/main"
+import { prisma } from "@/prisma_setup/main"
 import { createClient } from "@/lib/supabase/server"
 
-export default async function Profilepage(){
+console.log("database url", process.env.DATABASE_URL)
+
+export default async function Profilepage() {
+    console.log("database url", process.env.DATABASE_URL)
     const supabase = await createClient();
-    const {data:{user}} = await supabase.auth.getUser();
+    const { data: { user } } = await supabase.auth.getUser();
     const userdata = await prisma.user.findUnique({
-        where:{id:user?.id}
+        where: { id: user?.id }
     })
 
     if (!userdata) {
         return <div>User not found</div>;
     }
-    
 
-    return <Profile profileDetails={userdata}/>
+
+    return <Profile profileDetails={userdata} />
 }
